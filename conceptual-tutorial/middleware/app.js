86a602a456f1f9859062d40cb5bdcb5d7e40f8c8
michael <riedemanmj@gmail.com>
2023-06-30 15:24:34 -0700

    express middleware basics

---
+++ conceptual-tutorial/middleware/app.js
@@ -0,0 +1,52 @@
+import express from 'express';
+
+const app = express();
+
+//app.use() expectes a middleware to run, doesn't run to the request to the endpoint is sent
+
+
+//for better understanding of app.get middleware
+function middleware1 (req, res, next) {
+    console.log('I am middleware 1');
+    //if you don't call the next parameter the browser will just keep loading
+    next();
+}
+
+function middleware2 (req, res, next) {
+    console.log('I am middleware 2')
+    next();
+}
+
+function middleware3 (req, res, next) {
+    console.log('I am middleware 3')
+
+    //test errorHandler
+    const errObj = new Error('I am an error')
+    next(errObj);
+}
+
+function errorHandler (err, req, res, next) {
+    if (err) {
+        res.send('<h1>There was an error, please try again</h1>')
+    }
+}
+
+function standardExpressCallback (requestObject, responseObject, nextMiddleware) {
+    console.log("I am the standard Express function")
+    responseObject.send('<h1>Standard Express Callback</h1>');
+}
+
+app.use(middleware1)
+app.use(middleware2)
+
+app.get('/', middleware3, standardExpressCallback)
+
+//typical format you will see
+app.get('/hello', (req, res, next) => {
+    res.send('<h2>Hello World</h2>')
+});
+
+//order of middleware matters, so you need to put the errorHandler last
+app.use(errorHandler)
+
+app.listen(3000);
